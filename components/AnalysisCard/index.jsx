3083d427528ce993da3748d8f909788bb73e6ab7
versa-dev <tothefuture1117@gmail.com>
2022-04-23 00:01:56 +0800

    finish task

--- components/AnalysisCard/index.jsx
+++ components/AnalysisCard/index.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import s from "./AnalysisCard.module.css";
 
 const AnalysisCard = ({ data }) => {
@@ -7,7 +8,19 @@ const AnalysisCard = ({ data }) => {
         <div className={s.title}>{data.title}</div>
         <div className={s.value}>{data.value.toLocaleString()}</div>
       </div>
-      <div className={s.graph}>graph</div>
+      <div className={s.graph}>
+        <Image
+          src={
+            data.trend === "decrease"
+              ? "/assets/images/decrease.svg"
+              : data.trend === "increase"
+              ? "/assets/images/increase.svg"
+              : "/assets/images/average.svg"
+          }
+          height={95}
+          width={95}
+        />
+      </div>
     </div>
   );
 };
--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import { useState } from "react";
 import AnalysisCard from "../../components/AnalysisCard";
 import HomeCard from "../../components/HomeCard/HomeCard";
 import { ANALYSIS_DATA, MOCK_DATA } from "../../helper/const";
 import s from "./projects.module.css";
 
 const Dashboard = ({ favorite, analysis }) => {
+  const [search, setSearch] = useState("");
+
   return (
     <div className={s.root}>
       <div className={s.analysis}>
@@ -15,16 +18,25 @@ const Dashboard = ({ favorite, analysis }) => {
       <div className={s.favoriteHeader}>
         <div className={s.favoriteTitle}>Your favorites</div>
         <div className={s.searchBox}>
-          <input placeholder="Try 'Miami beachhour'" className={s.input} />
+          <input
+            placeholder="Try 'Miami beach house'"
+            className={s.input}
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
           <div className={s.search}>
             <Image src="/assets/images/search.svg" height={16} width={16} />
           </div>
         </div>
       </div>
       <div className={s.favorite}>
-        {favorite.map((item, index) => (
-          <HomeCard data={item} key={index} />
-        ))}
+        {favorite
+          .filter((item) =>
+            item.title.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((item, index) => (
+            <HomeCard data={item} key={index} />
+          ))}
       </div>
     </div>
   );
